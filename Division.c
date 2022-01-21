c9b83effa6d1a7d72019d51c83968df61e79d39e
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-01-21 12:45:14 +0530

    # include<stdio.h> int main() { int a,b,d; scanf("%d%d",&a,&b); if(1<=a,b&&a,b<=1000) {     if(a>b)    {        d=a/b;     printf("%d",d);    }    else    {     d=b/a;     printf("%d",d);    }
    }
     return 0;   
    }

---
+++ Division.c
@@ -0,0 +1,21 @@
+# include<stdio.h>
+int main()
+{
+int a,b,d;
+scanf("%d%d",&a,&b);
+if(1<=a,b&&a,b<=1000)
+{
+    if(a>b)
+   { 
+       d=a/b;
+    printf("%d",d);
+   }
+   else
+   {
+    d=b/a;
+    printf("%d",d);
+   }
+ 
+}
+ return 0;   
+}
