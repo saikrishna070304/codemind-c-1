213bb168826da7805bd7d2db58b63836b22c7923
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-04-22 18:49:09 +0530

    # include<stdio.h> int main() {     int n,r,s=0,k=1;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         s=s+r;         k=k*r;         n=n/10;     }     if(s==k)     printf("Spy Number");     else     printf("Not Spy Number");     return 0; }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,22 +1,18 @@
 # include<stdio.h>
 int main()
 {
-    int n,r,p=1,sum=0;
+    int n,r,s=0,k=1;
     scanf("%d",&n);
     while(n!=0)
     {
         r=n%10;
-        sum=sum+r;
-        p=p*r;
+        s=s+r;
+        k=k*r;
         n=n/10;
     }
-    if(sum==p)
-    {
-        printf("Spy Number");
-    }
+    if(s==k)
+    printf("Spy Number");
     else
-{
     printf("Not Spy Number");
-}
-return 0;
+    return 0;
 }
