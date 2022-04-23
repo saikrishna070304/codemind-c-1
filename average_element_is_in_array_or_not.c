41ecbe114d46be086c3eaf7edc119aa5236c305a
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-04-23 15:41:48 +0530

    # include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,sum=0,res,flag=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++) {     sum=sum+a[i]; } res=sum/n; for(i=0;i<n;i++) {     if(res==a[i])     flag=1; } if(flag==1) printf("True"); else printf("False"); return 0;
    }

---
+++ average_element_is_in_array_or_not.c
@@ -0,0 +1,28 @@
+# include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,sum=0,res,flag=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+{
+    sum=sum+a[i];
+}
+res=sum/n;
+for(i=0;i<n;i++)
+{
+    if(res==a[i])
+    flag=1;
+}
+if(flag==1)
+printf("True");
+else
+printf("False");
+return 0;
+    
+    
+}
