018dee6aeda483c18c4e77cb9c777f244aceb3e0
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-04-24 11:08:07 +0530

    # include<stdio.h> int main() {     int n,t;     scanf("%d",&n);     int a[n],i,flag=0;
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
        int d;
        for(i=0;i<n;i++)
        {
           if(a[i]%2!=0)
           {
               d=a[i];
           }
        }
        printf("%d",d);
        return 0;
    }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d",&n);
+    int a[n],i,flag=0;
+  
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int d;
+    for(i=0;i<n;i++)
+    {
+       if(a[i]%2!=0)
+       {
+           d=a[i];
+       }
+    }
+    printf("%d",d);
+    return 0;
+}
