ad8770002ef77d545098fb4c3d60032af7c63b6a
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-04-28 19:57:32 +0530

    #include<stdio.h> int main() {     char i,j;     int n,t;     scanf("%d",&n);     t=n;     while(t!=0)     {     for(i=65+n-1;i>=65;i--)     {         for(j=t;j>=1;j--)         printf("%c ",i);         printf(" ");         t--;
        }
        }
        return 0;
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n, i,j;
+    char i,j;
+    int n,t;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    t=n;
+    while(t!=0)
     {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",j);
-        }
+    for(i=65+n-1;i>=65;i--)
+    {
+        for(j=t;j>=1;j--)
+        printf("%c ",i);
         printf("
 ");
+        t--;
+    
+    }
     }
     return 0;
 }
