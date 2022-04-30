280837b074747f2e7895782f98a4504fd7b9a0e8
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-04-30 20:51:41 +0530

    #include<stdio.h> int main() {     int n,k;     scanf("%d%d",&n,&k);
        int a[n],i,p=0,sum=0;
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
       for(i=0;i<n;i++)
       {
           if(a[i]%k!=0)
           sum++;
       }
        printf("%d",sum);
    
        return 0;
    }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d%d",&n,&k);
+    
+    int a[n],i,p=0,sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+   for(i=0;i<n;i++)
+   {
+       if(a[i]%k!=0)
+       sum++;
+   }
+    printf("%d",sum);
+
+    return 0;
+}
