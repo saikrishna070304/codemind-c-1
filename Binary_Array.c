7abb47a9cb0b10043ec76ff735dd811f44d5eef7
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-05-19 20:39:47 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c=0;     for(i=0;i<n;i++)     {      if(a[i]==0 || a[i]==1)      c++;     }     if(c==n)     {         printf("True");     }     else     printf("False");     return 0; }

---
+++ Binary_Array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+     if(a[i]==0 || a[i]==1)
+     c++;
+    }
+    if(c==n)
+    {
+        printf("True");
+    }
+    else
+    printf("False");
+    return 0;
+}
