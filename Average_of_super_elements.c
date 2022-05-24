722f4d5d7095f2888686e07aa644b1b0ae3740e0
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-05-24 15:34:06 +0530

    #include<stdio.h> int counting(int n,int *b,int len) {     int i;     for(i=0;i<len;i++)     {       if(b[i]==n)       {           return 0;       }     }     return 1; } int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int k=0,j,flag=0,b[n],sum=0;     for(i=0;i<n;i++)     {         int c=1;         if(counting(a[i],b,n))         {             b[k]=a[i];             k++;             for(j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                 }             }             if(c==a[i])             {                 sum=sum+a[i];                 flag++;             }         }     }     if(flag==0)     {         printf("-1");     }     else     {         float av;         av=float(sum)/flag;         printf("%.2f",av);     }     return 0; }

---
+++ Average_of_super_elements.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int counting(int n,int *b,int len)
+{
+    int i;
+    for(i=0;i<len;i++)
+    {
+      if(b[i]==n)  
+      {
+          return 0;
+      }
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int k=0,j,flag=0,b[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        int c=1;
+        if(counting(a[i],b,n))
+        {
+            b[k]=a[i];
+            k++;
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+            if(c==a[i])
+            {
+                sum=sum+a[i];
+                flag++;
+            }
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        float av;
+        av=float(sum)/flag;
+        printf("%.2f",av);
+    }
+    return 0;
+}
